d85079f76d2f871641d093dc680b1041f5390d15
antodough <aemer004@ucr.edu>
2026-04-14 09:24:28 +0000

    3rd try

--- endianness_converter.sv
+++ endianness_converter.sv
@@ -1,24 +1,18 @@
 // Put your code below
 
 module endianness_converter(
-    input logic clk,
-    input logic [31:0] word,
-    input logic endianness_in,
-    input logic endianness_out,
+    input  logic        clk,
+    input  logic [31:0] word,
+    input  logic        endianness_in,
+    input  logic        endianness_out,
     output logic [31:0] converted_word
 );
 
-always_ff @(posedge clk) begin
-    if (endianness_in == endianness_out) begin
+always @(posedge clk) begin
+    if (endianness_in == endianness_out)
         converted_word <= word;
-    end else begin
-        converted_word <= {
-            word[7:0],
-            word[15:8],
-            word[23:16],
-            word[31:24]         
-        };
-    end
+    else
+        converted_word <= {word[7:0], word[15:8], word[23:16], word[31:24]};
 end
 
 endmodule
